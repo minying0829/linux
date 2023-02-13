5f6e34ec6cf2b6ede8c63ede972a25bffce0490c
Tomer Maimon <tmaimon77@gmail.com>
2023-02-13 12:53:57 +0200

    net: stmmac: Add SGMII NPCM8xx EVB revision A
    Add SGMII workaround for supporting SGMII NPCM8xx EVB revision A.
    
    Signed-off-by: Tomer Maimon <tmaimon77@gmail.com>

--- include/linux/stmmac.h
+++ include/linux/stmmac.h
@@ -257,6 +257,7 @@ struct plat_stmmacenet_data {
 	bool vlan_fail_q_en;
 	u8 vlan_fail_q;
 	unsigned int eee_usecs_rate;
+	bool arbel_rev_a;
 	struct pci_dev *pdev;
 	int int_snapshot_num;
 	int ext_snapshot_num;
