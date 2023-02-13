34d0bcdf3a6d48451c1d40d35cc8c63cb7eeaf22
Tomer Maimon <tmaimon77@gmail.com>
2023-02-13 12:30:01 +0200

    net: ethernet: stmmac: add sgmii support
    Signed-off-by: Tomer Maimon <tmaimon77@gmail.com>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_platform.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_platform.c
@@ -19,6 +19,8 @@
 
 #include "stmmac.h"
 #include "stmmac_platform.h"
+#define IND_AC_INDX	0x1FE
+#define SR_MII_CTRL	0x003E0000
 
 #ifdef CONFIG_OF
 
@@ -398,6 +400,8 @@ stmmac_probe_config_dt(struct platform_device *pdev, u8 *mac)
 	struct device_node *np = pdev->dev.of_node;
 	struct plat_stmmacenet_data *plat;
 	struct stmmac_dma_cfg *dma_cfg;
+	void __iomem *base;
+	u16 RegValue;
 	int phy_mode;
 	void *ret;
 	int rc;
@@ -490,6 +494,7 @@ stmmac_probe_config_dt(struct platform_device *pdev, u8 *mac)
 	if (of_device_is_compatible(np, "st,spear600-gmac") ||
 		of_device_is_compatible(np, "snps,dwmac-3.50a") ||
 		of_device_is_compatible(np, "snps,dwmac-3.70a") ||
+		of_device_is_compatible(np, "snps,npcm") ||
 		of_device_is_compatible(np, "snps,dwmac")) {
 		/* Note that the max-frame-size parameter as defined in the
 		 * ePAPR v1.1 spec is defined as max-frame-size, it's
@@ -621,6 +626,24 @@ stmmac_probe_config_dt(struct platform_device *pdev, u8 *mac)
 		goto error_hw_init;
 	}
 
+	if (of_device_is_compatible(np, "snps,npcm")) {
+		struct gpio_desc *gpio_evb_id[2];
+		u32 evb_id[2] = {0};
+
+		base = devm_platform_ioremap_resource(pdev, 1);
+		if (IS_ERR(base)) {
+			dev_warn(&pdev->dev, "devm_platform_ioremap_resource failed\n");
+		}
+		iowrite16((u16)(SR_MII_CTRL >> 9), base + IND_AC_INDX);
+		RegValue = ioread16(base + 0x2);
+		RegValue = ioread16(base + 0x0);
+		RegValue |= BIT(15);
+		iowrite16(RegValue, base + 0x0);
+		while (RegValue & BIT(15))
+			RegValue = ioread16(base + 0x0);
+		RegValue &= ~(BIT(12));
+		iowrite16(RegValue, base + 0x0);
+	}
 	return plat;
 
 error_hw_init:
