2b8f738c4c3934124ea6c7f7a3d1fbcd9701f68b
Stanley Chu <yschu@nuvoton.com>
2024-01-10 15:42:51 +0800

    i3c: expose interface for doing daa request
    Add device attribute "discover" for user space to request i3c
    master to do daa again.
    
    Example: request i3c0 to do discovery.
    echo 1 > /sys/bus/i3c/devices/i3c-0/discover
    
    Signed-off-by: Stanley Chu <yschu@nuvoton.com>

--- drivers/i3c/master.c
+++ drivers/i3c/master.c
@@ -524,6 +524,20 @@ static ssize_t i2c_scl_frequency_show(struct device *dev,
 }
 static DEVICE_ATTR_RO(i2c_scl_frequency);
 
+static ssize_t discover_store(struct device *dev, struct device_attribute *da,
+			      const char *buf, size_t count)
+{
+	struct i3c_master_controller *master;
+	ssize_t ret = count;
+
+	master = dev_to_i3cmaster(dev);
+	dev_dbg(&master->dev, "Request master to do DAA\n");
+	i3c_master_do_daa(master);
+
+	return ret;
+}
+static DEVICE_ATTR_WO(discover);
+
 static struct attribute *i3c_masterdev_attrs[] = {
 	&dev_attr_mode.attr,
 	&dev_attr_current_master.attr,
@@ -534,6 +548,7 @@ static struct attribute *i3c_masterdev_attrs[] = {
 	&dev_attr_pid.attr,
 	&dev_attr_dynamic_address.attr,
 	&dev_attr_hdrcap.attr,
+	&dev_attr_discover.attr,
 	NULL,
 };
 ATTRIBUTE_GROUPS(i3c_masterdev);
