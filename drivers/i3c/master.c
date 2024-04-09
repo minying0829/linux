9411faa17bcceaedb6bc99501c2f06ff0c345dcd
Stanley Chu <yschu@nuvoton.com>
2024-04-09 16:10:23 +0800

    net: mctp: Add I3C binding driver
    Provides MCTP network transport over an I3C bus, as specified in
    DMTF DSP0233.
    
    Each I3C bus (with "mctp-controller" devicetree property) gets an
    "mctpi3cX" net device created. I3C devices are reachable as remote
    endpoints through that net device. Link layer addressing uses the
    I3C PID as a fixed hardware address for neighbour table entries.
    
    The driver matches I3C devices that have the MIPI assigned DCR 0xCC for
    MCTP.
    
    Signed-off-by: Matt Johnston <matt@codeconstruct.com.au>

--- drivers/i3c/master.c
+++ drivers/i3c/master.c
@@ -21,6 +21,7 @@
 
 static DEFINE_IDR(i3c_bus_idr);
 static DEFINE_MUTEX(i3c_core_lock);
+static BLOCKING_NOTIFIER_HEAD(i3c_bus_notifier);
 static int __i3c_first_dynamic_bus_num;
 
 /**
@@ -457,6 +458,36 @@ static int i3c_bus_init(struct i3c_bus *i3cbus, struct device_node *np)
 	i3cbus->id = ret;
 
 	return 0;
+}
+
+void i3c_for_each_bus_locked(int (*fn)(struct i3c_bus *bus, void *data),
+			     void *data)
+{
+	struct i3c_bus *bus;
+	int id;
+
+	mutex_lock(&i3c_core_lock);
+	idr_for_each_entry(&i3c_bus_idr, bus, id)
+		fn(bus, data);
+	mutex_unlock(&i3c_core_lock);
+}
+EXPORT_SYMBOL_GPL(i3c_for_each_bus_locked);
+
+int i3c_register_notifier(struct notifier_block *nb)
+{
+	return blocking_notifier_chain_register(&i3c_bus_notifier, nb);
+}
+EXPORT_SYMBOL_GPL(i3c_register_notifier);
+
+int i3c_unregister_notifier(struct notifier_block *nb)
+{
+	return blocking_notifier_chain_unregister(&i3c_bus_notifier, nb);
+}
+EXPORT_SYMBOL_GPL(i3c_unregister_notifier);
+
+static void i3c_bus_notify(struct i3c_bus *bus, unsigned int action)
+{
+	blocking_notifier_call_chain(&i3c_bus_notifier, action, bus);
 }
 
 static const char * const i3c_bus_mode_strings[] = {
@@ -2849,6 +2880,8 @@ int i3c_master_register(struct i3c_master_controller *master,
 	if (ret)
 		goto err_del_dev;
 
+	i3c_bus_notify(i3cbus, I3C_NOTIFY_BUS_ADD);
+
 	/*
 	 * We're done initializing the bus and the controller, we can now
 	 * register I3C devices discovered during the initial DAA.
@@ -2881,6 +2914,8 @@ EXPORT_SYMBOL_GPL(i3c_master_register);
  */
 void i3c_master_unregister(struct i3c_master_controller *master)
 {
+	i3c_bus_notify(&master->bus, I3C_NOTIFY_BUS_REMOVE);
+
 	i3c_master_i2c_adapter_cleanup(master);
 	i3c_master_unregister_i3c_devs(master);
 	i3c_master_bus_cleanup(master);
@@ -3032,6 +3067,8 @@ int i3c_target_register(struct i3c_master_controller *master, struct device *par
 	if (ret)
 		goto err_cleanup_bus;
 
+	i3c_bus_notify(i3cbus, I3C_NOTIFY_BUS_ADD);
+
 	return 0;
 
 err_cleanup_bus:
@@ -3046,6 +3083,8 @@ EXPORT_SYMBOL_GPL(i3c_target_register);
 
 int i3c_target_unregister(struct i3c_master_controller *master)
 {
+	i3c_bus_notify(&master->bus, I3C_NOTIFY_BUS_REMOVE);
+
 	i3c_target_unregister_i3c_dev(master);
 	i3c_target_bus_cleanup(master);
 	device_unregister(&master->dev);
